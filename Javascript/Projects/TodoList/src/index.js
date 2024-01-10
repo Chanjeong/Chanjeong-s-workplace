5be272f1c353cf941628f143a6535abdf379ffc6
chanjeong park <ckswjd9595@gmail.com>
2024-01-10 17:51:23 +0900

    chanjeong

--- Javascript/Projects/TodoList/src/index.js
+++ Javascript/Projects/TodoList/src/index.js
@@ -2,8 +2,8 @@ import _ from "lodash";
 import "./style.css";
 
 class Project {
-  constructor(name) {
-    this.name = name;
+  constructor(type) {
+    this.type = type;
     this.todos = [];
     this.projects = [];
   }
@@ -16,13 +16,13 @@ class Project {
     return this.projects;
   }
 
-  updateProject(newName){
+  updateProject(newName) {
     this.name = newName;
   }
 
   deleteProject(projectToDelete) {
     const index = this.projects.indexOf(projectToDelete);
-    if(index !== -1){
+    if (index !== -1) {
       this.projects.splice(index, 1);
     }
   }
@@ -33,8 +33,8 @@ class Project {
 
   deleteTodo(todoToDelete) {
     const index = this.todos.indexOf(todoToDelete);
-    if(index !== -1){
-      this.projects.splice(index, 1);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
     }
   }
 }
@@ -60,26 +60,93 @@ class Todos {
   }
 }
 
-class Interface {
-  constructor(){
+class UserStart {
+  constructor() {
+    this.main = document.createElement("div");
+    this.main.id = "main";
     this.title = document.createElement("div");
     this.title.id = "interface-title";
-    this.main = document.createElement("div");
-    this.main.id = "interface-main";
-    this.createProjectButton = document.createElement("div");
-    this.createProjectButton.id = "create-project-button";
-    this.createTodoButton = document.createElement("div");
-    this.createTodoButton.id = "create-todo-button";
+    this.started = document.createElement("button");
+    this.started.id = "start-interface";
 
-    document.body.append(this.title, this.main, this.createProjectButton, this.createTodoButton);
+    this.title.innerHTML = `<h1>Welcome to the Todoism</h1>
+    <h2>Let's make your life systematically!</h2>`;
 
-    this.createProjectButton.addEventListener("click", ()=>{
+    this.started.innerHTML = "Let's get started";
 
+    this.started.addEventListener("click", () => {
+      this.switchTab(new UserMain());
     });
 
-    this.createTodoButton.addEventListener("click", ()=>{
-
-    });
+    this.main.append(this.title, this.started);
   }
 
+  switchTab(moduleInstance) {
+    const main = document.getElementById("main");
+    main.innerHTML = "";
+    main.append(moduleInstance.main);
+  }
 }
+
+class UserMain {
+  constructor() {
+    this.user = new UserStart();
+    this.main = document.createElement("div");
+    this.main.id = "main";
+    this.createProjectButton = document.createElement("button");
+    this.createProjectButton.id = "create-project-button";
+    this.createTodoButton = document.createElement("button");
+    this.createTodoButton.id = "create-todo-button";
+
+    this.createProjectButton.innerHTML = `Create Project`;
+    this.createTodoButton.innerHTML = `Create Todo`;
+
+    this.createProjectButton.addEventListener("click", () => {
+      this.createProject();
+    });
+
+    this.createTodoButton.addEventListener("click", () => {
+      this.createTodo();
+    });
+
+    this.main.append(this.createProjectButton, this.createTodoButton);
+  }
+
+  createProject() {
+    const projectDialog = document.createElement("dialog");
+    projectDialog.id = "project-dialog";
+
+    const storeProject = () => {
+      const typeInput = document.getElementById("type");
+      const projectType = typeInput.value;
+
+      if (projectType) {
+        const newProject = new Project(projectType);
+        newProject.createProject();
+        alert(`Project type "${projectType}" has been created! `);
+        document.getElementById("project-dialog").close();
+      } else {
+        alert("Please enter a project type!");
+      }
+    };
+
+    projectDialog.innerHTML = `<label for="type">Type</label>
+    <input type="text" name="type" id="type" required />
+    <button id="createButton">Create</button>
+    <button autofocus id="closeButton">Close</button>`;
+
+    const createButton = projectDialog.querySelector("#createButton");
+    const closeButton = projectDialog.querySelector("#closeButton");
+
+    createButton.addEventListener("click", storeProject);
+    closeButton.addEventListener("click", () => {
+      projectDialog.close();
+    });
+
+    document.body.appendChild(projectDialog);
+    projectDialog.showModal();
+  }
+}
+
+const user = new UserStart();
+document.body.appendChild(user.main);
